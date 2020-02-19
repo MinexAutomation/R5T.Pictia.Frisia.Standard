7b762f8e605c1e23790fd6281dcb13bab91b1ba4
MinexAutomation <minex.automation@gmail.com>
2020-02-19 01:02:46 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Pictia.Frisia.Standard.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Pictia.Frisia.Standard.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Pictia.Frisia.Standard/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Pictia.Frisia.Standard
+{
+    public class Class1
+    {
+    }
+}
