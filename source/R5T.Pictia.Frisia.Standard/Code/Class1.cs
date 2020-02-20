db06b820574796586ac731dc786b3d0b299aa0a1
MinexAutomation <minex.automation@gmail.com>
2020-02-20 01:44:14 -0800

    Added IServiceCollection extensions.

--- source/R5T.Pictia.Frisia.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Pictia.Frisia.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Pictia.Frisia.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Frisia.Standard;
+
+
+namespace R5T.Pictia.Frisia.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="ISftpClientWrapperProvider"/> service.
+        /// </summary>>
+        public static IServiceCollection AddSftpClientWrapperProvider(this IServiceCollection services, string hostFriendlyName)
+        {
+            services.AddFrisiaSftpClientWrapperProvider(
+                services.AddAwsEc2ServerSecretsProviderAction(hostFriendlyName));
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ISftpClientWrapperProvider"/> service.
+        /// </summary>>
+        public static ServiceAction<ISftpClientWrapperProvider> AddSftpClientWrapperProviderAction(this IServiceCollection services, string hostFriendlyName)
+        {
+            var serviceAction = new ServiceAction<ISftpClientWrapperProvider>(() => services.AddSftpClientWrapperProvider(hostFriendlyName));
+            return serviceAction;
+        }
+    }
+}
